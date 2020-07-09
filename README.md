6c9e2c4f55d0da54ff9c1b88fb7c42db1ad89076
Monkeylord <bluegod_001@hotmail.com>
2020-07-09 16:33:02 +0800

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # XposedFridaBridge
+## 介绍 Introduction
+
 A frida script implement XposedBridge &amp; load xposed modules, without installing xposed framwork.
+
+在不安装Xposed Framework的情况下，通过Frida使用Xposed插件。
+
+## 用法 Usage
+
+### 准备工作 Preparation
+
+1. 将XposedBridge.jar推入设备中 push XposedBridge.jar into device
+2. 安装插件或将插件APK推入设备中 install modules or push module apks into device
+3. 配置插件列表 configure modules.list
+
+~~~shell
+adb push XposedBridge.jar /data/local/tmp/XposedBridge.jar
+adb install module.apk
+adb shell 'echo "/data/app/demo.xposedmodule-1/base.apk" > /data/local/tmp/conf/modules.list' 
+~~~
+
+`/data/local/tmp/conf/modules.list`与XposedInstaller沙箱下的`/conf/modules.list`相同，其格式是每行一个APK路径，插件APK可安装，也可不安装。路径指向/data/local/tmp中的APK也可以。
+
+### 开始使用 Load Modules
+
+~~~shell
+frida -U [target app process] -l XposedFridaBridge.js
+~~~
+
+## 已知问题 Known Issues
+
+在Nexus5设备上测试通过，可以使用justtrustme等插件，但是部分模拟器可能会有兼容性问题，比如夜神，`com.android.org.conscrypt.TrustManagerImpl`中的`checkServerTrusted`不能被Frida Hook，Hook之后会崩溃。
---
+++ XposedFridaBridge.js
@@ -0,0 +1,316 @@
+/*
+    Frida Xposed Bridge
+    by Monkeylord
+    License: MIT
+    
+    Load Xposed Bridge&Modules though Frida.
+    
+    原理：通过Frida加载XposedBridge.jar，同时通过Frida Java Hook来实现Xposed API。随后模拟Xposed初始化，并加载插件，然后再模拟应用启动。
+*/
+
+function implementXposedAPI(){
+    // Implement ZygoteService API
+    var ZygoteService = Java.classFactory.use("de.robv.android.xposed.services.ZygoteService")
+    
+    ZygoteService.checkFileAccess.implementation = function(){
+        console.log("[API Call] checkFileAccess", " filename:", arguments[0])
+        return true
+    }
+    
+    ZygoteService.statFile.implementation = function(){
+        console.log("[API Call] statFile", " filename:", arguments[0])
+        return null
+    }
+    
+    ZygoteService.readFile.overload('java.lang.String').implementation = function(){
+        console.log("[API Call] readFile", " filename:", arguments[0])
+        return null
+    }
+    
+    // Implement XposedBridge API
+    var XposedBridge =  Java.classFactory.use("de.robv.android.xposed.XposedBridge")
+    XposedBridge.runtime.value = 2  // Art
+    XposedBridge.hadInitErrors.implementation=function(){
+        console.log("[API Call] hadInitErrors")
+        return false
+    }
+    
+    XposedBridge.getStartClassName.implementation=function(){
+        console.log("[API Call] getStartClassName")
+        // TODO
+        return ""
+    }
+    
+    XposedBridge.getRuntime.implementation=function(){
+        console.log("[API Call] getRuntime")
+        // 1 = Dalvik, 2 = Art
+        return 2
+    }
+    
+    XposedBridge.startsSystemServer.implementation=function(){
+        console.log("[API Call] startsSystemServer")
+        // TODO
+        return false
+    }
+    
+    XposedBridge.getXposedVersion.implementation=function(){
+        console.log("[API Call] getXposedVersion")
+        return 82
+    }
+    
+    XposedBridge.initXResourcesNative.implementation=function(){
+        console.log("[API Call] initXResourcesNative")
+        // Disable Resource Hook
+        // TODO: implement Resource Hook
+        return false
+    }
+    
+    XposedBridge.hookMethodNative.implementation=function(javaReflectedMethod, jobject, jint, javaAdditionalInfo){
+        console.log("[API Call] hookMethodNative", javaReflectedMethod.getDeclaringClass().getName(), javaReflectedMethod.getName())
+        
+        // 这里来的，可能是Method，也可能是Constructor
+        var refMethod = Java.use(javaReflectedMethod.getClass().getName())
+        var method = Java.cast(javaReflectedMethod, refMethod)
+               
+        // 创建GlobalRef，不然再次调用时可能就是野指针了
+        javaReflectedMethod.$h = Java.vm.getEnv().newGlobalRef(javaReflectedMethod.$h)
+        javaAdditionalInfo.$h = Java.vm.getEnv().newGlobalRef(javaAdditionalInfo.$h)
+        
+        
+        // 拿基本信息
+        
+        // Frida中Method Hook和Constructor Hook方式不同，所以要区分
+        var clazz = method.getDeclaringClass().getName()
+        var mtdname = (javaReflectedMethod.getClass().getName()=="java.lang.reflect.Constructor")? "$init": method.getName()
+        var overload = method.getParameterTypes().map(function(clz){return clz.getName()})
+        
+        var fridaMethod = Java.use(clazz)[mtdname].overload.apply(Java.use(clazz)[mtdname], overload)
+        
+        fridaMethod.implementation = function(){
+            console.log("handleHookedMethod", javaReflectedMethod.getDeclaringClass().getName(), javaReflectedMethod.getName())
+            var isInstanceMethod = fridaMethod.type == 3    // 3 = Instance Method
+            var thisObject = null
+            if (isInstanceMethod)
+                thisObject = this
+            
+            var args = arguments
+            var jarr = Object.keys(arguments).map(function(key){return args[key]})
+            
+            fridaMethod.argumentTypes.forEach(function(type,index){
+                var env = Java.vm.getEnv()
+                jarr[index] = Java.classFactory._getType("java.lang.Object").fromJni(type.toJni(jarr[index], env),env, false)
+            })
+            
+            try{
+                var xposedResult = XposedBridge.handleHookedMethod(javaReflectedMethod, jint, javaAdditionalInfo, thisObject, Java.array("java.lang.Object", jarr))
+                /*
+                    Frida-java在这里有Bug，手动解决数组对象问题
+                */
+                var env = Java.vm.getEnv()
+                var retType = fridaMethod._p[4]
+                var hhmRetType = XposedBridge.handleHookedMethod.overloads[0]._p[4]
+                
+                return retType.fromJni(hhmRetType.toJni(xposedResult, env), env, false)
+            }catch(e){
+                console.log("Exception: ", e)
+                throw e
+            }
+        }
+    }
+    
+    XposedBridge.setObjectClassNative.implementation=function(javaObj, javaClazz){
+        console.log("[API Call] setObjectClassNative", javaObj, javaClazz)
+        Java.cast(javaObj, javaClazz)
+    }
+    
+    XposedBridge.dumpObjectNative.implementation=function(){
+        console.log("[API Call] dumpObjectNative")
+        return undefined
+    }
+    
+    XposedBridge.cloneToSubclassNative.implementation=function(javaObj, javaClazz){
+        console.log("[API Call] cloneToSubclassNative", javaObj, javaClazz)
+        return Java.cast(javaObj, javaClazz)
+    }
+    
+    XposedBridge.removeFinalFlagNative.implementation=function(){
+        console.log("[API Call] removeFinalFlagNative")
+        // TODO: Remove final flag
+        // This is used by Resource Hook
+        // Reference: https://github.com/frida/frida-java-bridge/blob/master/lib/android.js#L1390
+    }
+    
+    XposedBridge.invokeOriginalMethodNative.implementation = function(javaMethod, isResolved, jobjectArray, jclass, javaReceiver, javaArgs){
+        console.log("[API Call] invokeOriginalMethodNative", javaMethod)
+        
+        var refMethod = Java.use(javaMethod.getClass().getName())
+        var method = Java.cast(javaMethod, refMethod)
+        var clazz = method.getDeclaringClass().getName()
+        var mtdname = method.getName()
+        var overload = method.getParameterTypes().map(function(clz){return clz.getName()})
+        
+        var fridaMethod = Java.use(clazz)[mtdname].overload.apply(Java.use(clazz)[mtdname], overload)
+        var thisObject = (fridaMethod.type == 3)?javaReceiver:Java.use(clazz)
+        
+        var jarr = javaArgs
+        // 不知道为什么结尾可能会多一个null，可能是ducktape问题？手动去掉。
+        jarr = jarr.slice(0, javaArgs.length)
+        
+        fridaMethod.argumentTypes.forEach(function(type,index){
+            var env = Java.vm.getEnv()
+            jarr[index] = type.fromJni(Java.classFactory._getType("java.lang.Object").toJni(jarr[index], env), env, false)
+        })
+        
+        var result = null
+        try{
+            result = fridaMethod.apply(thisObject, jarr)
+            /*
+                这里有Frida的Bug，frida-java-bridge的对象处理中，数组并不被认为是Object，所以不能作为Object返回。
+                因为数组经过了转换，转换的代码在 https://github.com/frida/frida-java-bridge/blob/4aa88501d2c6c871ada1c696816ca6f7f2626d7b/lib/types.js#L396
+                而数组处理需要ArrayType来处理，ObjectType并不兼容它
+                
+                需要手动解决这个问题，构造和析构对象。
+            */
+            var env = Java.vm.getEnv()
+            var retType = fridaMethod._p[4]
+            var iomnRetType = XposedBridge.invokeOriginalMethodNative.overloads[0]._p[4]
+            
+            var rawResult = retType.toJni(result, env)
+            var tmpResult = iomnRetType.fromJni(rawResult, env, false)
+            result = tmpResult
+            
+        }catch(e){
+            console.log(e)
+            throw e
+        }
+        return result || null
+    }
+    
+    XposedBridge.closeFilesBeforeForkNative.implementation=function(){
+        console.log("[API Call] closeFilesBeforeForkNative")
+        // TODO
+        // Useless outside Zygote
+    }
+    
+    XposedBridge.reopenFilesAfterForkNative.implementation=function(){
+        console.log("[API Call] reopenFilesAfterForkNative")
+        // TODO
+        // Useless outside Zygote
+    }
+    
+    XposedBridge.invalidateCallersNative.implementation=function(){
+        console.log("[API Call] invalidateCallersNative")
+        // TODO: 
+        // This is used in resource hook
+    }
+}
+
+function FrameworkInit(bridgePath, xposedPath){
+    var ActivityThread = Java.use("android.app.ActivityThread")
+    var apkClassloader = ActivityThread.currentActivityThread().peekPackageInfo(ActivityThread.currentApplication().getPackageName(), true).getClassLoader()
+
+    console.log("[XposedFridaBridge] Current Application Classloader: ",apkClassloader)
+    
+    // 加载Xposed类
+    Java.openClassFile(bridgePath).load()
+
+    var XposedCL = Java.classFactory.loader
+    console.log("[XposedFridaBridge] Xposed Classloader: ", XposedCL)
+
+    // 实现XposedBridge API
+    implementXposedAPI()
+    
+    console.log("[XposedFridaBridge] XposedBridge successfully loaded\n")
+    
+    // 开始处理初始化
+    console.log("[XposedFridaBridge] Initating Xposed Framework")
+
+    // 模拟XposedBridge.main
+    var XposedBridge =  Java.classFactory.use("de.robv.android.xposed.XposedBridge")
+    // XposedBridge.initXResources()    //initXResource被放弃实现，转而通过对XposedBridge.jar二次打包实现，修改了android.content.res.XResource
+    XposedBridge.XPOSED_BRIDGE_VERSION.value = 82
+    XposedBridge.BOOTCLASSLOADER.value = XposedCL
+    XposedBridge.isZygote.value = true
+    
+    var XposedInit = Java.classFactory.use("de.robv.android.xposed.XposedInit")
+    XposedInit.BASE_DIR.value = xposedPath
+    console.log("[XposedFridaBridge] Static Attribute Set")
+    
+    console.log("[XposedFridaBridge] Initating SELinuxHelper")
+    var SELinuxHelper = Java.classFactory.use("de.robv.android.xposed.SELinuxHelper")
+    SELinuxHelper.initOnce()
+    SELinuxHelper.initForProcess(ActivityThread.currentApplication().getPackageName())
+
+    console.log("[XposedFridaBridge] hookResources")
+    XposedInit.hookResources()
+    console.log("[XposedFridaBridge] initForZygote")
+    XposedInit.initForZygote()
+    console.log("[XposedFridaBridge] Framework Initated\n")
+    
+    console.log("[XposedFridaBridge] Load Modules")
+    XposedInit.loadModules()
+    
+    console.log("[XposedFridaBridge] Xposed Framework Ready\n")
+}
+
+function triggerLoadPackage(){
+    
+    var XposedBridge =  Java.classFactory.use("de.robv.android.xposed.XposedBridge")
+    var XCallback = Java.classFactory.use("de.robv.android.xposed.callbacks.XCallback")
+    var LoadPackageParam = Java.classFactory.use("de.robv.android.xposed.callbacks.XC_LoadPackage$LoadPackageParam")
+    
+    console.log("[XposedFridaBridge] Preparing LoadPackageParam")
+    var ActivityThread = Java.use("android.app.ActivityThread")
+    var app = ActivityThread.currentApplication()
+    var thread = ActivityThread.currentActivityThread()
+    console.log(" [PackageName]", app.getPackageName())
+    console.log(" [ProcessName]", ActivityThread.currentPackageName())
+    var boundApplication = thread.mBoundApplication.value
+    console.log(" [boundApplication]", boundApplication)
+    var appInfo = boundApplication.appInfo.value
+    console.log(" [AppInfo]", appInfo)
+    //console.log(appInfo.packageName.value)
+    var compatInfo = boundApplication.compatInfo.value
+    console.log(" [compatInfo]", compatInfo)
+    var loadedApk = thread.getPackageInfoNoCheck(appInfo, compatInfo)
+    console.log(" [loadedApk]", loadedApk)
+    var classLoader = loadedApk.getClassLoader()
+    console.log(" [classLoader]", classLoader)
+    
+    var lpparam = LoadPackageParam.$new(XposedBridge.sLoadedPackageCallbacks.value)
+    lpparam.packageName.value = app.getPackageName()
+    lpparam.processName.value = ActivityThread.currentPackageName()
+    lpparam.classLoader.value = loadedApk.getClassLoader()
+    lpparam.appInfo.value = loadedApk.getApplicationInfo()
+    lpparam.isFirstApplication.value = true
+    
+    console.log("[XposedFridaBridge] LoadPackageParam Ready\n")
+    console.log("[XposedFridaBridge] Triggering Modules")
+    XCallback.callAll(lpparam)
+}
+
+// 启动
+Java.performNow(function(){
+    // Java.deoptimizeEverything()
+    console.log("Xposed Frida Bridge\n by Monkeylord\n")
+    
+    console.log("XposedBridge.jar Path:", "/data/local/tmp/XposedBridge.jar")
+    console.log("modules.list Path:", "/data/local/tmp/conf/modules.list")
+    console.log("\n")
+    
+    console.log("[XposedFridaBridge] Start Loading Xposed")
+    
+    // 获取当前应用Application
+    var ActivityThread = Java.use("android.app.ActivityThread")
+    var app = ActivityThread.currentApplication()
+    
+    // 初始化Framework
+    console.log("[XposedFridaBridge] Current Application: ", app.getPackageName())
+    FrameworkInit("/data/local/tmp/XposedBridge.jar", "/data/local/tmp/")
+        
+    // 触发模块启动
+    console.log("[XposedFridaBridge] Triggering Modules Load")
+    triggerLoadPackage()
+    
+    console.log("[XposedFridaBridge] Ready\n")
+})
